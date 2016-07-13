47d4d15d7a35271a8f997c7679d5410bdc4eb4fb
Skyfler <skyflerr@gmail.com>
2016-07-13 16:19:55 +0300

    Prebuild HTML page files now placed in './dev/' instead of './dev/templates/'. HTML building gulp task now places output files into './public/' instead of './'. Now all used images placed in './dev/img/used/'. PSD and AI files of sprites were moved into './dev/img/_samples/'. Gulp tasks for image minification, public folder clean, starting webserver for preview were addded. README.md was added. Public filder removed from git repositiory.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,35 @@ var gulp = require('gulp'),
     pngquant = require('imagemin-pngquant'),
     fileinclude = require('gulp-file-include'),
     htmlmin = require('gulp-htmlmin'),
+    browserSync = require("browser-sync"),
+    rimraf = require('rimraf'),
     rename = require('gulp-rename');
+
+var config = {
+    server: {
+        baseDir: "./public"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Skyfler"
+};
+
+var path = {
+    public: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: './public/',
+        js: './public/scripts/',
+        css: './public/css/',
+        img: './public/img/'
+    },
+    dev: { //Пути откуда брать исходники
+        html: './dev/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: './dev/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: './dev/css/style.css',
+        img: './dev/img/used/**/*.*' //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+    },
+    clean: './public'
+};
 
 gulp.task('webpack', function() {
     return gulp.src('./dev/scripts/page.js')
@@ -21,34 +49,25 @@ gulp.task('webpack', function() {
         .pipe(gulp.dest('./public/scripts/'));
 });
 
-gulp.task('minifyCss', function () {
+gulp.task('css', function () {
     gulp.src('./dev/css/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./public/css/'));
 });
 
-/*gulp.task('image', function () {
-    return gulp.src('./new/*')
+gulp.task('image', function () {
+    return gulp.src(path.dev.img)
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('./new'));
-});*/
+        .pipe(gulp.dest(path.public.img));
+});
 
-gulp.task('concatHtml', function() {
-    gulp.src([
-        './dev/templates/index.html',
-        './dev/templates/about_us.html',
-        './dev/templates/med.html',
-        './dev/templates/services.html',
-        './dev/templates/prices.html',
-        './dev/templates/important.html',
-        './dev/templates/contacts.html',
-        './dev/templates/article.html'
-    ])
+gulp.task('html', function() {
+    gulp.src(path.dev.html)
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
@@ -58,7 +77,15 @@ gulp.task('concatHtml', function() {
             processConditionalComments: true,
             removeComments: true
         }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest(path.public.html));
 });
 
-gulp.task('default', ['concatHtml', 'minifyCss', 'webpack']);
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', ['html', 'css', 'webpack', 'image']);
